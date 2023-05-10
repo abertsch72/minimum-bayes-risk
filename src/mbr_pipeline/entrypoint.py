e3d30f3af85a5a1762fb0f24601e25c517350976
Amanda Bertsch <abertsch72@gmail.com>
2023-05-10 13:42:24 -0400

    debug wandb

--- src/mbr_pipeline/entrypoint.py
+++ src/mbr_pipeline/entrypoint.py
@@ -10,8 +10,8 @@ from choose_dataset import DatasetArgs, get_dataset
 from args import Args, load_args
 
 parser = ArgumentParser()
-parser.add_argument("config_file", type=str, help="file containing config for pipeline arguments", required=True)
-parser.add_argument("no_wandb", default=False, action="store_true", help="set this to avoid logging to wandb (will override config)", required=False)
+parser.add_argument("--config_file", type=str, help="file containing config for pipeline arguments", required=True)
+parser.add_argument("--no_wandb", default=False, action="store_true", help="set this to avoid logging to wandb (will override config)", required=False)
 
 setup = parser.parse_args()
 
@@ -22,7 +22,7 @@ args.pipeline.wandb = (not setup.no_wandb)
 
 print(args.pipeline.wandb)
 if args.pipeline.wandb: 
-    wandb.init(project="gormleylab/lattice-search", name=args.pipeline.run_name, group=args.pipeline.wandb_group, config=args.to_dict())
+    wandb.init(entity="gormleylab", project="lattice-decoding", name=args.pipeline.run_name, group=args.pipeline.wandb_group, config=args.to_dict())
 
 random.seed(args.PipelineArgs.seed)
 
@@ -56,7 +56,7 @@ args.pipeline.wandb = (not setup.no_wandb)
 
 print(args.pipeline.wandb)
 if args.pipeline.wandb: 
-    wandb.init(project="gormleylab/lattice-search", name=args.pipeline.run_name, group=args.pipeline.wandb_group, config=args.to_dict())
+    wandb.init(entity="gormelylab", project="lattice-decoding", name=args.pipeline.run_name, group=args.pipeline.wandb_group, config=args.to_dict())
 
 random.seed(args.PipelineArgs.seed)
 
