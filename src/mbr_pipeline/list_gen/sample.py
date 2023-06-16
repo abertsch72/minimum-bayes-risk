4980fbd42a25c906f1902879bb2ee3d6d33132ab
axie66 <alex.xie7734@gmail.com>
2023-06-15 21:03:48 -0400

    merge from refactor; fix scoring for sampling methods

--- src/mbr_pipeline/list_gen/sample.py
+++ src/mbr_pipeline/list_gen/sample.py
@@ -2,10 +2,14 @@ import warnings
 from typing import Any, Callable, Generator, Tuple
 
 import numpy as np
+import torch
+import torch.nn.utils.rnn as rnn_utils
 from tqdm import tqdm
-from transformers import PreTrainedModel
+from transformers import BartForConditionalGeneration, PreTrainedModel
 
 from mbr_pipeline.list_gen.lattice import Lattice
+
+NUM_GEN_AT_ONCE = 100
 
 
 class SamplingMethods:
@@ -18,11 +22,13 @@ class SamplingMethods:
         num_beams,
         num_beam_groups=1,
         diversity_penalty=0.0,
-        do_sample=False,
+        stochastic=False,
+        memoryless=False,
+        beam_temp=1.0,
+        beam_top_p=1.0,
     ):
         return model.generate(
             input_ids,
-            do_sample=do_sample,
             max_length=max_length,
             num_beams=num_beams,
             num_return_sequences=num_seqs,
@@ -33,24 +39,110 @@ class SamplingMethods:
         )
 
     @staticmethod
-    def model_sample(input_ids, model, num_seqs, max_length, temp, top_p, sample_unique_seqs=False):
+    def stochastic_beam_search(
+        input_ids,
+        model: BartForConditionalGeneration,
+        num_seqs,
+        max_length,
+        num_beams,
+        num_beam_groups=1,
+        diversity_penalty=0.0,
+        stochastic=True,
+        memoryless=False,
+        beam_temp=1.0,
+        beam_top_p=1.0,
+    ):
         return model.generate(
             input_ids,
-            do_sample=True,
             max_length=max_length,
-            top_p=top_p,
-            temperature=temp,
+            num_beams=num_beams,
             num_return_sequences=num_seqs,
+            num_beam_groups=num_beam_groups,
+            diversity_penalty=diversity_penalty,
+            top_p=beam_top_p,
+            temperature=beam_temp,
             output_scores=True,
             return_dict_in_generate=True,
         )
 
     @staticmethod
-    # TODO: fix args
-    def lattice_mbr():
-        from src.mbr_pipeline.list_gen.lattice_mbr import run_lattice_mbr
+    @torch.no_grad()
+    def model_sample(
+        input_ids, model, num_seqs, max_length, temp, top_p, sample_unique_seqs=False
+    ):
+        if num_seqs <= NUM_GEN_AT_ONCE:
+            return model.generate(
+                input_ids,
+                do_sample=True,
+                max_length=max_length,
+                num_beams=1,
+                num_return_sequences=num_seqs,
+                num_beam_groups=1,
+                top_p=top_p,
+                temperature=temp,
+                output_scores=True,
+                return_dict_in_generate=True,
+            )
+        else:
+            num_generated = 0
+            outputs = []
+            max_len = -1
+            while num_generated < num_seqs:
+                curr_num_seqs = min(NUM_GEN_AT_ONCE, num_seqs - num_generated)
+                partial_output = model.generate(
+                    input_ids,
+                    do_sample=True,
+                    max_length=max_length,
+                    num_beams=1,
+                    num_return_sequences=curr_num_seqs,
+                    num_beam_groups=1,
+                    top_p=top_p,
+                    temperature=temp,
+                    output_scores=True,
+                    return_dict_in_generate=True,
+                )
+                # for key in partial_output.keys():
+                #     t = partial_output[key]
+                #     if isinstance(t, torch.Tensor):
+                #         partial_output[key] = t.cpu()
+                outputs.append(partial_output)
+                num_generated += curr_num_seqs
+                max_len = max(max_len, len(partial_output.scores))
+            full_sequences = torch.full(
+                (num_seqs, max_len + 1), model.config.pad_token_id
+            )
+            start_idx = 0
+            for out in outputs:
+                curr_sequences = out.sequences
+                curr_bs, curr_len = curr_sequences.shape
+                full_sequences[
+                    start_idx : start_idx + curr_bs, :curr_len
+                ] = curr_sequences
+                start_idx += curr_bs
+            # beam_indices = torch.cat([out.beam_indices for out in outputs], dim=0)
+            scores = []
+            for i in range(max_len):
+                scores.append(
+                    torch.cat(
+                        [
+                            out.scores[i]
+                            if i < len(out.scores)
+                            else torch.full(
+                                out.scores[0].shape,
+                                -float("inf"),
+                                device=out.scores[0].device,
+                            )
+                            for out in outputs
+                        ],
+                        dim=0,
+                    )
+                )
+            output = partial_output[0]
+            output.sequences = full_sequences
+            # output.beam_indices = beam_indices
+            output.scores = tuple(scores)
 
-        return run_lattice_mbr()
+            return output
 
 
 def listgen(
@@ -63,7 +155,7 @@ def listgen(
     strategy_args,
     model: PreTrainedModel = None,
     lattices: Generator[Tuple[Lattice, Any], None, None] = None,
-    sample_unique_list = False,
+    sample_unique_list=False,
 ):
     all_hypos = []
 
@@ -85,7 +177,7 @@ def listgen(
                     num_seqs=num_seqs,
                     output=output,
                     max_length=max_length,
-                    **strategy_args
+                    **strategy_args,
                 )
             )
     else:
@@ -94,10 +186,13 @@ def listgen(
         for i in tqdm(range(len(dataset))):
             dp = dataset["input"][i]
             max_source_len = 0
-            try:
-                max_source_len = model.config.max_position_embeddings
-            except:
-                max_source_len = model.config.n_positions
+
+            max_source_len = getattr(model.config, "max_position_embeddings", None)
+            if max_source_len is None:
+                max_source_len = getattr(model.config, "n_positions", None)
+            if max_source_len is None:
+                max_source_len = getattr(model.config, "max_length", None)
+            assert max_source_len is not None
 
             input_ids = tokenizer.encode(
                 dp,
@@ -114,59 +209,72 @@ def listgen(
                     model,
                     num_seqs=num_seqs,
                     max_length=max_length,
-                    **strategy_args
+                    **strategy_args,
                 )  #
 
                 # get sequence scores by summing generated token scores and applying length penality
                 # Tip: recomputing the scores is only guaranteed to match with `normalize_logits=False`.
                 scores_on_cpu = tuple(score.cpu() for score in outputs.scores)
-                try:
+                if hasattr(outputs, "beam_indices"):  # beam search
                     transition_scores = model.compute_transition_scores(
                         outputs.sequences.cpu(),
                         scores_on_cpu,
                         outputs.beam_indices.cpu(),
                         normalize_logits=False,
                     ).numpy()
-                except:
+
+                    # this way of recomputing scores only works for beam search
+                    output_length = input_ids.shape[0] + np.sum(
+                        transition_scores < 0, axis=1
+                    )
+                    length_penalty = model.generation_config.length_penalty
+                    reconstructed_scores = (
+                        np.sum(transition_scores, axis=1)
+                        / (output_length**length_penalty)
+                    ).tolist()
+                else:  # not beam search
                     transition_scores = model.compute_transition_scores(
-                        outputs.sequences.cpu(), scores_on_cpu, normalize_logits=False
+                        outputs.sequences.cpu(), scores_on_cpu, normalize_logits=True
                     ).numpy()
 
-                output_length = input_ids.shape[0] + np.sum(transition_scores < 0, axis=1)
-                length_penalty = model.generation_config.length_penalty
-                reconstructed_scores = (
-                    np.sum(transition_scores, axis=1) / (output_length**length_penalty)
-                ).tolist()
+                    transition_scores[np.isinf(transition_scores)] = 0.0
+                    output_length = input_ids.shape[0] + np.sum(
+                        1 - np.isinf(transition_scores), axis=1
+                    )
+                    length_penalty = model.generation_config.length_penalty
+                    reconstructed_scores = (
+                        np.sum(transition_scores, axis=1)
+                        / (output_length**length_penalty)
+                    ).tolist()
 
                 # todo: hash outputs.sequences
                 outputs_decoded = tokenizer.batch_decode(
                     outputs.sequences, skip_special_tokens=True
                 )
-                
+
                 if sample_unique_list:
                     # TODO: track total number of samples
-                    # TODO: dedup at token level instead 
+                    # TODO: dedup at token level instead
                     scores_for_unq = dict(zip(outputs_decoded, reconstructed_scores))
-                    previous_unq = dict(zip(outputs["hypos"], outputs["reconstructed_scores"]))
-                    scores_for_unq.update(previous_unq) # overrides any duplicates here with the older version
+                    previous_unq = dict(
+                        zip(outputs["hypos"], outputs["reconstructed_scores"])
+                    )
+                    scores_for_unq.update(
+                        previous_unq
+                    )  # overrides any duplicates here with the older version
                     # debug
-                    print(f"The total number of new hypotheses is {len(scores_for_unq) - len(previous_unq)} for a total of {len(scores_for_unq)} generated!")
+                    print(
+                        f"The total number of new hypotheses is {len(scores_for_unq) - len(previous_unq)} for a total of {len(scores_for_unq)} generated!"
+                    )
                     outputs_decoded = scores_for_unq.keys()
-                    reconstructed_scores = [scores_for_unq[output] for output in outputs_decoded]
+                    reconstructed_scores = [
+                        scores_for_unq[output] for output in outputs_decoded
+                    ]
 
                 outputs["hypos"] = outputs["hypos"].extend(outputs_decoded)
                 outputs["lprobs"] = outputs["lprobs"].extend(reconstructed_scores)
 
                 return outputs
-            
-            outputs = {
-                "document": dp,
-                "gold": dataset["output"][i],
-                "id": dataset["id"][i],
-                "hypos": [],
-                "lprobs": [],
-                }  
-
 
             while len(outputs["hypos"]) < num_seqs:
                 outputs = continue_list_gen(outputs)
